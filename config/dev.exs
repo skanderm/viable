69e25898d1b7232c3878710086e10d34e2d709a4
Skander Mzali <skanderm@gmail.com>
2022-02-19 16:30:21 -0800

    Add working tailwind basics

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :viable, ViableWeb.Endpoint,
   secret_key_base: "/gp312KlBhusuUsdTGUBtTGEV39xhqA8h0YmvW2sGH6oLw1BBGzT6T/y2fLdEdqO",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
